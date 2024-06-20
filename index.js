10ddf0d675e58249ad447684157db5d40449ab4b
Cristhian Rodríguez <crisrodam1996@gmail.com>
2024-06-20 01:33:01 -0500

    Crear categoria

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-import server from './src/server.js'
-import { PORT } from './src/config/config.js'
-import { conn } from './src/lib/connection.js'
-const port = PORT ?? 3000
+import server from "./src/server.js";
+import { PORT } from "./src/config/config.js";
+import { conn } from "./src/lib/connection.js";
+const port = PORT ?? 3000;
 conn
   .sync({ logging: false, force: false })
   .then(() => {
-    console.log('Base de datos conectada')
+    console.log("Base de datos conectada");
     server.listen(port, () => {
-      console.log(`Server listening in: http://localhost:${port}`)
-    })
+      console.log(`Server listening in: http://localhost:${port}`);
+    });
   })
   .catch((err) => {
-    console.log('Error en la conexión: ', err.message)
-  })
+    console.log("Error en la conexión: ", err.message);
+  });
--- src/controllers/categoria.controllers.js
+++ src/controllers/categoria.controllers.js
@@ -1,14 +1,40 @@
-import { Categoria } from '../lib/connection.js'
+import { Op } from "sequelize";
+import { Categoria } from "../lib/connection.js";
+
+const crearCategoria = async (req, res) => {
+  try {
+    const { nombre } = req.body;
+    const [_, created] = await Categoria.findOrCreate({
+      where: {
+        nombre: {
+          [Op.like]: `%${nombre}%`,
+        },
+      },
+      defaults: { nombre },
+    });
+    return created
+      ? req.status(200).json({
+          message: "Categoría creada con éxito",
+        })
+      : res.status(400).json({
+          message: "Esta categoría ya existe",
+        });
+  } catch (error) {
+    return req.status(500).json({
+      message: "Error interno en el servidor",
+    });
+  }
+};
 
 const listarCategorias = async (req, res) => {
   try {
-    const categorias = await Categoria.findAll({})
-    return res.status(200).json({ categorias })
+    const categorias = await Categoria.findAll({});
+    return res.status(200).json({ categorias });
   } catch (error) {
     return req.status(500).json({
-      message: 'Error interno en el servidor',
-    })
+      message: "Error interno en el servidor",
+    });
   }
-}
+};
 
-export default { listarCategorias }
+export default { listarCategorias, crearCategoria };
